169062327537079805c8920ba0d961a234bbb00b
rivet9658 <59174409+rivet9658@users.noreply.github.com>
2023-07-24 18:49:11 +0800

    feat: docker setting update ✨

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+# 第一次部屬會執行資料遷移及創建一超級使用者
+if [ ! -f /.firstrun ]; then
+	touch /.firstrun
+    python manage.py makemigrations
+    python manage.py migrate
+    echo "from django.contrib.auth.models import User; User.objects.create_superuser('superadmin', 'superadmin@gmail.com', '1qaz@WSX3edc')" | python manage.py shell
+fi
+
+python manage.py runserver 0.0.0.0:8000
